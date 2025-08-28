7808c6f3d8f70be1045ffb607b32fd17adaba470
wilder06 <wilder.pacpac@usil.pe>
2025-08-28 10:19:00 -0500

    feat/HU002: Agregar configuracion incial servicio registrar solicitud

---
+++ domain/model/src/main/java/pe/com/creditya/model/applications/Applications.java
@@ -0,0 +1,14 @@
+package pe.com.creditya.model.applications;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Applications {
+}
---
+++ domain/usecase/src/main/java/pe/com/creditya/usecase/applications/ApplicationsUseCase.java
@@ -0,0 +1,6 @@
+package pe.com.creditya.usecase.applications;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class ApplicationsUseCase {
+}
